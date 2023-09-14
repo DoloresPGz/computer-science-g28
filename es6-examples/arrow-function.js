4e1358b25ed32ee949dffdc3d412a6f18535e6e7
DoloresPGz <dolores.pgz@gmail.com>
2023-09-13 21:08:04 -0600

    ejemplos de arrow function

---
+++ es6-examples/arrow-function.js
@@ -0,0 +1,22 @@
+function saludar(){
+    console.log("Hola")
+}
+
+//saludar()
+
+const saludo = function () {
+    console.log("Hola")
+}
+
+//saludo()
+
+const suma = (a, b) => {
+    console.log("algo")
+    return a + b
+}
+
+//console.log(suma(2,3))
+
+const potencia = a => a**3
+
+console.log(potencia(2))
