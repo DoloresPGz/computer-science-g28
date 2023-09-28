4ba245faf4df8bdbdc419680bc4950ddbf1befef
DoloresPGz <dolores.pgz@gmail.com>
2023-09-27 19:16:47 -0600

    base de la lista enlazada

---
+++ estructuras-datos/lista-enlazada.js
@@ -0,0 +1,20 @@
+class Node{
+    constructor(data){
+        this.data = data
+        this.next = null //puntero
+    }
+}
+
+class LinkedList{
+    constructor(head = null){
+        this.head = head
+    }
+
+    //Inserción de un elemento
+
+    //Borrado de un elemento
+
+    //Recorrido de la lista
+
+    //Búsqueda de un elemento
+}
