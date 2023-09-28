5290d5c1ad6e6ca5c1096332ce08ba2ad07571f8
DoloresPGz <dolores.pgz@gmail.com>
2023-09-27 19:43:06 -0600

    se creó el método de insertar elementos a la ll

--- estructuras-datos/lista-enlazada.js
+++ estructuras-datos/lista-enlazada.js
@@ -11,6 +11,23 @@ class LinkedList{
     }
 
     //Inserción de un elemento
+    insert(data){
+        //Creo mi nuevo nodo que se va a enlazar a mi estructura
+        let node = new Node(data)
+
+        //Primer caso: Mi lista se encuentra vacía (head -> null)
+        if(this.head == null){
+            this.head = node
+        }else{
+        //Segundo caso: Tengo por lo menos un nodo en la lista
+            let current = this.head
+            while(current.next !== null){
+                let lo_que_era_current = current
+                current = lo_que_era_current.next
+            }
+            current.next = node
+        }
+    }
 
     //Borrado de un elemento
 
@@ -18,3 +35,11 @@ class LinkedList{
 
     //Búsqueda de un elemento
 }
+
+let ll = new LinkedList()
+
+ll.insert(1)
+ll.insert(2)
+ll.insert(3)
+
+console.log(ll)
