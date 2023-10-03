60d7ddfdcf022986fc3e8d4666f45db0995c17b5
Francisco Campos <sasasasamaes13@gmail.com>
2023-10-02 19:29:24 -0600

    comentarios en codigo

--- algoritmos-avanzados/busqueda.js
+++ algoritmos-avanzados/busqueda.js
@@ -4,12 +4,15 @@
 //              y devuelve su posicion
 console.time('busquedaLineal')
 function busquedaLineal(arreglo, elemento){
-  
+  // Recorrer el arreglo
   for(let i = 0; i < arreglo.length; i++){
+    // Si el elemento del arreglo es igual al elemento buscado
     if(arreglo[i] === elemento){
+      // Devolver el indice
       return i
     }
   }
+  // Si no se encontro el elemento
   return "no se encontro" // No se encontro el elemento
 }
 console.timeEnd('busquedaLineal')
@@ -20,23 +23,28 @@ console.timeEnd('busquedaLineal')
 //              y devuelve su posicion
 console.time('busquedaBinaria')
 function busquedaBinaria(arreglo, elemento){
-
+  // Definir el inicio y el fin del arreglo
   let inicio = 0
   let fin = arreglo.length -1
-
+  // Mientras el inicio sea menor o igual al fin
   while(inicio <= fin){
+    // Definir el punto medio
     let medio = Math.floor((inicio + fin) / 2)
-
+    // Si el elemento del punto medio es igual al elemento buscado
     if(arreglo[medio] === elemento){
+      // Devolver el punto medio
       return  medio
+      // Si el elemento del punto medio es menor al elemento buscado
     } else if(arreglo[medio] < elemento){
+      // Definir el inicio como el punto medio + 1
       inicio = medio + 1
-    } else {
+    } else { 
+      // Definir el fin como el punto medio - 1
       fin = medio - 1
     }
   }
-  return "No se encontro" // No se encontro el elemento
-
+  // Si no se encontro el elemento
+  return "No se encontro" // No se encontro el elemento\
 }
 console.timeEnd('busquedaBinaria')
 
